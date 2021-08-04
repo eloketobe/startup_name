525cbedda6354e9c9230d904f3c8d96fc9f9318b
eloketobe <sameloke@yahoo.com>
2021-08-04 15:33:35 +0100

    create favourite icon

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,33 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = TextStyle(fontSize: 18);
+  final _saved = <WordPair>{};
 
-  Widget _buildRow(WordPair pair) => ListTile(
-        title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        ),
-      );
+  Widget _buildRow(WordPair pair) {
+    final _alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        _alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: _alreadySaved ? Colors.red : Colors.grey,
+      ),
+      onTap: () {
+        setState(() {
+          if (_alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+        for (WordPair pair in _saved) {
+          print(pair);
+        }
+      },
+    );
+  }
 
   Widget _buildSuggestions() => ListView.builder(itemBuilder: (context, i) {
         if (i.isOdd) return Divider();
@@ -40,9 +60,7 @@ class _RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
-        for (WordPair pair in _suggestions) {
-          print(pair);
-        }
+
         return _buildRow(_suggestions[index]);
       });
 
